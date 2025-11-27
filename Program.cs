aa1efbc7ee143eafac28736cdf0e143a1f965563
Sammy Nava <sammy@sammynava.com>
2025-11-27 10:43:37 -0600

    Add Identity Endpoints & Admin Policy to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 
 using AeonRegistryAPI.Endpoints.Home;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,20 @@ var connectionString = DataUtility.GetConnectionString(builder.Configuration);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
 
+// Add Identity endpoints
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+}).AddRoles<IdentityRole>()
+  .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Add an Admin policy
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+
+// Enable validation for minimal APIs (This is a .NET 10 feature)
+builder.Services.AddValidation();  // Enforces [Required] and other data annotations
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,8 +41,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // This allows serving static files like images from wwwroot folder
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Map Endpoints
+var authRouteGroup = app.MapGroup("/api/auth")
+    .WithTags("Admin");
+authRouteGroup.MapIdentityApi<ApplicationUser>();
 app.MapHomeEndpoints();
 
 app.Run();
