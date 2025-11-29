6c95861e27a830ad8a3fbf57fa89aef7cb323851
Sammy Nava <sammy@sammynava.com>
2025-11-29 17:50:01 -0600

    Add Fogot Password Endpoint
    Added the custom forgot password endpoint. Refactored the Enpoint class into two classes; one for endpoints, one for handlers.

--- Endpoints/CustomIdentity/CustomIdentityEndpoints.cs
+++ Endpoints/CustomIdentity/CustomIdentityEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using AeonRegistryAPI.Endpoints.CustomIdentity.Models.Requests;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
@@ -10,81 +12,40 @@ public static class CustomIdentityEndpoints
 {
     public static IEndpointRouteBuilder MapCustomIdentityEndpoints(this IEndpointRouteBuilder route)
     {
-        // Step 1:  Make a Grouop for Custom Identity Endpoints
+        // Make a Grouop for Custom Identity Endpoints
         var group = route.MapGroup("/api/auth")
             .WithTags("Admin");
 
-        // Step 2:  Map Endpoints in the group to their handlers
-        group.MapPost("/register-admin", RegisterUser)
+        // Map Endpoints in the group to their handlers
+        group.MapPost("/register-admin", CustomIdentityHandlers.RegisterUser)
             .WithName("RegisterAdmin")
             .WithSummary("Register a User")
             .WithDescription("Registerrs a user must have admin role.")
-            //.RequireAuthorization("AdminPolicy");
-            ;
-        // Step 3:  Implement Route Handlers in CustomIdentityHandlers class
-        // (see below)
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest);
+        //.RequireAuthorization("AdminPolicy");
+        ;
 
-        // Step 4:  Add OpenAPI metadata to each endpoint
+        group.MapPost("reset-password", CustomIdentityHandlers.ResetPassword)
+            .WithName("ResetPassword")
+            .WithDescription("Custom Reset Password for a user")
+            .WithSummary("Custom Reset Password")
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest);
 
-        // Step 5:  Return the modified IEndpointRouteBuilder        
+        group.MapPost("forgot-password", CustomIdentityHandlers.ForgotPassword)
+            .WithName("ForgotPassword")
+            .WithDescription("Custom Forgot Password flow")
+            .WithSummary("Custom Forgot Password")
+            .Produces(StatusCodes.Status200OK)
+            .Produces(StatusCodes.Status400BadRequest);
+
+
+        // TODO: Step 3:  Implement Route Handlers in CustomIdentityHandlers class        
+
+        // TODO: Step 4:  Add OpenAPI metadata to each endpoint
+
+        // TODO: Step 5:  Return the modified IEndpointRouteBuilder        
         return route;
-    }
-
-    
-    private static async Task<IResult> RegisterUser(
-        RegisterUserRequest dto,
-        UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager,
-        IEmailSender emailSender,
-        IConfiguration config
-        )
-    {
-        // see if the user already exists
-        if(await userManager.FindByEmailAsync(dto.Email) is not null)
-        {
-            return Results.BadRequest(new { Error = $"A User with email {dto.Email} already exists"});
-        }
-
-        // Make a new ApplicationUser
-        ApplicationUser user = new() 
-        {
-            UserName = dto.Email,
-            Email = dto.Email,
-            FirstName = dto.FirstName,
-            LastName = dto.LastName            
-        };
-        string tempPassword = "Abc@123!"; // pwd must meet complexity rules
-        var created = await userManager.CreateAsync(user, tempPassword);
-        if(!created.Succeeded)
-        {
-            return Results.BadRequest(new { Error = $"Could not create user {dto.Email} ", Details = created.Errors });
-        }
-
-        // Generate a password reset token
-        var resetToken =  await userManager.GeneratePasswordResetTokenAsync(user);
-        var encodeToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(resetToken));
-
-        // Put the user in the Researcher role
-        if (await roleManager.RoleExistsAsync("Researcher"))
-        {
-           await userManager.AddToRoleAsync(user, "Researcher");
-        }
-
-        // Send the user an email with their temp password to change password
-        var baseURl = config["BaseUrl"] ?? "https://localhost:7023";
-        await emailSender.SendEmailAsync(
-            dto.Email,
-            "Your New Account",
-            
-            $"""
-
-            Your account has been created.  Please change your password by visiting:  {baseURl}/SetPasswrod.html
-
-            {baseURl}/Setpassword.html?email={dto.Email}&resetCode={encodeToken}
-
-            """
-            );
-
-        return Results.Ok(new { Message = $"User, {dto.Email}, created. Passsword reset email sent."});
     }
 }
---
+++ Endpoints/CustomIdentity/CustomIdentityHandlers.cs
@@ -0,0 +1,160 @@
+﻿using AeonRegistryAPI.Endpoints.CustomIdentity.Models.Requests;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Security.Principal;
+using System.Text;
+
+namespace AeonRegistryAPI.Endpoints.CustomIdentity;
+
+public static class CustomIdentityHandlers
+{
+    public static async Task<IResult> RegisterUser(
+        RegisterUserRequest request,
+        UserManager<ApplicationUser> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IEmailSender emailSender,
+        IConfiguration config)
+    {
+        // see if the user already exists
+        if (await userManager.FindByEmailAsync(request.Email) is not null)
+        {
+            return Results.BadRequest(new { Error = $"A User with email {request.Email} already exists" });
+        }
+
+        // Make a new ApplicationUser
+        ApplicationUser user = new()
+        {
+            UserName = request.Email,
+            Email = request.Email,
+            FirstName = request.FirstName,
+            LastName = request.LastName
+        };
+        string tempPassword = "Abc@123!"; // pwd must meet complexity rules
+        var created = await userManager.CreateAsync(user, tempPassword);
+        if (!created.Succeeded)
+        {
+            return Results.BadRequest(new { Error = $"Could not create user {request.Email} ", Details = created.Errors });
+        }
+
+        // Generate a password reset token
+        var resetToken = await userManager.GeneratePasswordResetTokenAsync(user);
+        var encodeToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(resetToken));
+
+        // Put the user in the Researcher role
+        if (await roleManager.RoleExistsAsync("Researcher"))
+        {
+            await userManager.AddToRoleAsync(user, "Researcher");
+        }
+
+        // Send the user an email with their temp password to change password
+        var baseURl = config["BaseUrl"] ?? "https://localhost:7023";
+        var setPwdLink = $"{baseURl}/Setpassword.html?email={request.Email}&resetCode={encodeToken}";
+
+        await EmailLink(emailSender, request.Email,"Your New Account",
+            "Your account has been created. Please change your password by visiting...",
+            setPwdLink);
+
+        //await emailSender.SendEmailAsync(
+        //    request.Email,
+        //    "Your New Account",
+
+        //    $"""
+
+        //    Your account has been created.  Please change your password by visiting:  {baseURl}/SetPasswrod.html
+
+        //    {baseURl}/Setpassword.html?email={request.Email}&resetCode={encodeToken}
+
+        //    """
+        //    );
+
+        return Results.Ok(new { Message = $"User, {request.Email}, created. Passsword reset email sent." });
+    }
+
+    public static async Task<IResult> ResetPassword(
+        ResetPasswordRequest request,
+        UserManager<ApplicationUser> userManager)
+    {
+        // Validate the request
+        if (string.IsNullOrEmpty(request.Email)
+         || string.IsNullOrEmpty(request.ResetCode)
+         || string.IsNullOrEmpty(request.NewPassword))
+        {
+            return Results.BadRequest(new { Message = "All fields are required" });
+        }
+
+        // Find the User
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if (user is null)
+        {
+            return Results.BadRequest(new { Message = "user not found." });
+        }
+
+        // Attempt to reset the password
+        try
+        {
+            var decodeToken = Encoding.UTF8.GetString(
+                WebEncoders.Base64UrlDecode(request.ResetCode));
+
+            var result = await userManager.ResetPasswordAsync(user, decodeToken, request.NewPassword);
+            if (!result.Succeeded)
+                return Results.BadRequest(new { Message = "Unable to reset password." });
+
+            return Results.Ok(new { Message = "Password successfully reset." });
+        }
+        catch (FormatException)
+        {
+            return Results.BadRequest(new { Message = "Invalid Token" });
+        }
+        catch (Exception ex)
+        {
+            return Results.BadRequest(new { Message = $"Error: {ex.Message}" });
+        }
+    }
+
+    public static async Task<IResult> ForgotPassword(
+        ForgotPasswordRequest request,
+        UserManager<ApplicationUser> userManager,
+        IEmailSender emailSender,
+        IConfiguration config)
+    {
+        if(string.IsNullOrEmpty(request.Email))
+            return Results.BadRequest(new { Message = "Email is required." });
+
+        var user = await userManager.FindByEmailAsync(request.Email);
+        if(user is null)
+            return Results.Ok(new { Message = "If the user existes a forgot password link will be sent." });
+
+        // Generate a password reset token
+        var token = await userManager.GeneratePasswordResetTokenAsync(user);
+        var encodeToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(token));
+
+        // Send the user an email with their temp password to change password
+        var baseURl = config["BaseUrl"] ?? "https://localhost:7023";
+        var resetLink = $"{baseURl}/reset-password.html?email={user.Email}&resetCode={encodeToken}";
+
+        await EmailLink(emailSender,request.Email,
+            "Forgot Password",
+            "To reset your password, click on this link...",
+            resetLink);
+        
+        return Results.Ok(new { Message = "If the user existes a forgot password link will be sent." });
+    }
+
+    private static async Task EmailLink(IEmailSender emailSender, [EmailAddress] string email, string subject, string body, string resetLink)
+    {
+        await emailSender.SendEmailAsync(
+            email,
+            $"{subject}",
+            $"""
+
+            {body}
+
+            {resetLink}
+
+            """
+            );
+    }
+
+}
